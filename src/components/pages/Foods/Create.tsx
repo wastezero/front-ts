3edbb64d385a94480315ea4e87d78e3cb9514025
dauletisatayev <dake.99.10@gmail.com>
2021-03-15 17:05:21 +0600

    feat: file upload

--- src/components/pages/Foods/Create.tsx
+++ src/components/pages/Foods/Create.tsx
@@ -3,6 +3,7 @@ import Button from "@src/components/atoms/Button";
 import RestaurantsDropdown from "@src/components/organisms/RestaunrantsDropdown";
 import { useHistory } from "react-router-dom";
 import * as React from "react";
+import { firebaseapp } from "@src/utils/firebase";
 
 interface IFoodCreateProps {}
 
@@ -37,6 +38,15 @@ const FoodCreate: React.FunctionComponent<IFoodCreateProps> = (props) => {
         history.push("/account/foods");
       })
       .catch(() => setLoading(false));
+  };
+
+  const onUploadFile = (e) => {
+    const file = e.target.files[0];
+    const storageRef = firebaseapp.storage().ref();
+    const fileRef = storageRef.child(file.name);
+    fileRef.put(file).then(() => {
+      fileRef.getDownloadURL().then(setImage);
+    });
   };
 
   return (
@@ -116,13 +126,9 @@ const FoodCreate: React.FunctionComponent<IFoodCreateProps> = (props) => {
               Image
             </label>
             <input
-              required
-              placeholder="Enter Image url"
+              type="file"
+              onChange={onUploadFile}
               className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-              value={image}
-              onChange={(ev) => {
-                setImage(ev.target.value);
-              }}
             />
           </div>
           <div className="mt-6">
